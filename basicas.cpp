4543b12bac41239d295a6e764bec34fbbe05db71
Jahirxp9 <153339585+Jahirxp9@users.noreply.github.com>
2023-12-08 22:34:47 -0500

    Update basicas.cpp

--- basicas.cpp
+++ basicas.cpp
@@ -56,9 +56,7 @@ int menu(){
         gotoxy(5, 9); cout << "|| 1. Mostrar datos del la univesidad ||";
         gotoxy(5, 10); cout <<"|| 2. Control de Cursos               ||";
         gotoxy(5, 11); cout <<"|| 3. Control Estudiantes             ||";
-        /*gotoxy(5, 12); cout <<"|| 4. Control Docentes                ||";*/
         gotoxy(5, 13); cout <<"|| 5. Control Docente               ||";
-       /* gotoxy(5, 14); cout <<"|| 6. Control Ventas                  ||";*/
         gotoxy(5, 15); cout <<"|| 7. Salir                           ||";
         gotoxy(5, 16); cout <<"|| Elije una opcion:                  || ";
         gotoxy(5, 17); cout <<"||====================================||";
@@ -108,24 +106,6 @@ int menuTrabajadores(){
     return opc;
 }
 
-/* int menuButacas(){
-    int opc;
-    do{
-        system("cls");
-    changeTextColor ( FOREGROUND_GREEN | FOREGROUND_INTENSITY );
-        gotoxy(45, 6); cout << "||==========================||";
-        gotoxy(45, 7); cout << "||      MENU DE DOCENTES    ||";
-        gotoxy(45, 8); cout << "||==========================||";
-        gotoxy(45, 9); cout << "|| 1. Registrar Docente      ||";
-        gotoxy(45, 10); cout <<"|| 2. Lista Docente          ||";
-        gotoxy(45, 11); cout <<"|| 3. Buscar Docente        ||";
-        gotoxy(45, 12); cout <<"|| 4. Regresar              ||";
-        gotoxy(45, 13); cout <<"|| Elije una opcion:        || ";
-        gotoxy(45, 14); cout <<"||==========================||";
-        gotoxy(45, 15); cout <<"->";
-        cin >> opc;
-    }while(!( opc > 0 && opc < 5 ));
-    return opc; */
 
 int menuClientes(){
     int opc;
@@ -145,28 +125,7 @@ int menuClientes(){
         cin >> opc;
     }while(!( opc > 0 && opc <5 ));
     return opc;
-}/*
-int menuVentas(){
-    int opc;
-    do{
-        system("cls");
-    changeTextColor ( FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY );
-        gotoxy(45, 6); cout << "||==========================||";
-        gotoxy(45, 7); cout << "||      MENU DE VENTAS      ||";
-        gotoxy(45, 8); cout << "||==========================||";
-        gotoxy(45, 9); cout << "|| 1. Registrar Venta       ||";
-        gotoxy(45, 10); cout <<"|| 2. Listar ventas         ||";
-        gotoxy(45, 11); cout <<"|| 3. Listar ventas por dia ||";
-        gotoxy(45, 12); cout <<"|| 4. Buscar venta          ||";
-        gotoxy(45, 13); cout <<"|| 5. Regresar              || ";
-        gotoxy(45, 14); cout <<"|| Elije una opcion:        || ";
-        gotoxy(45, 15); cout <<"||==========================||";
-        gotoxy(45, 16); cout <<"->";
-        cin >> opc;
-    }while(!( opc > 0 && opc < 6 ));
-    return opc;
 }
-*/
 
 string leerTexto(string mensaje){
     string valor;
@@ -194,19 +153,7 @@ float leercorreo2(string mensaje ){
     }while(!( valor > 0 && valor<10000));
     return valor;
 }
-/*
-void leerGenero(string mensaje ){
-    string valor;
-    do{
-        cout << mensaje;
-        fflush( stdin );
-        getline( cin, valor );
-    }while(!( valor =="H" || valor == "M" ));
-    if ( valor == "H" )
 
-        return ;
-}
-*/
 float leerPrecio( string mensaje ){
     float valor;
     do{
@@ -253,26 +200,4 @@ void encabezadoVenta(){
     gotoxy(108,5);cout <<"Codigo butaca";
     gotoxy(123,5);cout <<"Precio butaca";
 }
-
-/*
-void encabezadoVentaFecha(){
-    gotoxy(10,5);cout <<"Codigo";
-    gotoxy(18,5);cout <<"Apellidos trabajador";
-    gotoxy(42,5);cout <<"DNI trabajador";
-    gotoxy(60,5);cout <<"Apellidos cliente";
-    gotoxy(80,5);cout <<"DNI cliente";
-    gotoxy(95,5);cout <<"Codigo butaca";
-    gotoxy(110,5);cout <<"Precio butaca";
-}
-
-void encabezadoButacas(){
-    system ("cls");
-    gotoxy(30,2); cout<<"Lista Docente";
-    gotoxy(17,3); cout<<"Nombre";
-    gotoxy(26,3); cout<<"Correo";
-
-}
-
-*/
-
 
